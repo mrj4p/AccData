56813f3cbb50659863e670989c3525402e3131aa
Kimutai <edwardsigey@gmail.com>
2025-04-27 18:35:06 +0300

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -14,7 +14,7 @@ async function fetchWithCORS(url, options = {}) {
     const response = await fetch(url, {
       ...options,
       headers: {
-        Authorization: process.env.API_SECRET,
+        // Authorization: process.env.API_SECRET,
         'Content-Type': 'application/json',
         ...options.headers
       },
